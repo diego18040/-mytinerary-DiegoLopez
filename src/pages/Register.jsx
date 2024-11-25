9a16b803c28a5e20b0a8f43fa42d4ae1bc8d5c3c
diego18040 <dieg00.lopez00@hotmail.com>
2024-11-25 02:49:06 -0600

    Agregamos Google

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -206,6 +206,18 @@ const SignUpForm = () => {
             >
               Register
             </button>
+            {/* Register with Google */}
+            <button
+              onClick={signUpWithGoogle}
+              className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+            >
+              <img
+                src="https://www.svgrepo.com/show/355037/google.svg"
+                className="w-6 h-6"
+                alt="Google Logo"
+              />
+              <span>Register with Google</span>
+            </button>
           </form>
         </div>
       </div>
